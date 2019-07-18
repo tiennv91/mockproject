7ee84831ab558326f7fca84ec673e725077e4f9b
longdang0497 <danghoanglong3004@gmail.com>
2019-07-19 00:42:01 +0700

    Updating config in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -103,7 +103,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { :host => "https://suitojapan.herokuapp.com" }
+  config.action_mailer.default_url_options = { :host => "https://localhost:3000" }
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
   # config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
