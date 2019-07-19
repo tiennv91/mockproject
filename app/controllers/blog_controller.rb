13f0af2ca0064cec7bfdff57de1a34cc87271c90
duongtrang2701 <duong1997trang@gmail.com>
2019-07-19 11:03:29 +0700

    Update search form

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -17,6 +17,7 @@ class BlogController < ApplicationController
     @search = Blog.ransack(params[:q])
     @categories = Category.all
     @locations = Location.all
+    @hashtags = Hashtag.all
     @search.sorts = 'blog_details.title desc' if @search.sorts.empty?
     @blogs = @search.result(distinct: true).order(created_at: :DESC).page(params[:page]).per(3)
 
@@ -35,6 +36,7 @@ class BlogController < ApplicationController
     @bl_im1 = Blog.all.order(impressions_count: :DESC).first
     @bl_im2 = Blog.all.order(impressions_count: :DESC).second
     @bl_im3 = Blog.all.order(impressions_count: :DESC).third
+    @hashtags = Hashtag.all
 
     # breacrumb
     add_breadcrumb 'Blog', :blog_index_path
@@ -44,15 +46,13 @@ class BlogController < ApplicationController
   def search
     unless params[:q][:categories_category_name_in].nil?
       params[:q][:categories_category_name_in] = params[:q][:categories_category_name_in].split(",")
-      unless params[:q][:location_province_in].nil?
-        params[:q][:location_province_in] = params[:q][:location_province_in].split(",")
-        unless params[:q][:hashtags_tag_name_in].nil?
-          params[:q][:hashtags_tag_name_in] = params[:q][:hashtags_tag_name_in].split(" ")
-        end
-      end
     end
-    
-    
+    unless params[:q][:hashtags_tag_name_in].nil?
+      params[:q][:hashtags_tag_name_in] = params[:q][:hashtags_tag_name_in].split(",")
+    end
+    unless params[:q][:location_province_in].nil?
+      params[:q][:location_province_in] = params[:q][:location_province_in].split(",")
+    end
     index
     render :index
   end
