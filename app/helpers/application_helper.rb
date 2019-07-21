af5bbd14b09224b2173873629182055df3f1521d
dangnghia1997 <dangnghia25197@gmail.com>
2019-07-21 23:15:30 +0700

    modify applycation.html.erb with multiple process if is_multiple_process_form? in experience page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def hide_side_bar?
     controller_name == 'experience' && action_name == 'application_form'
   end
+
+  def is_multiple_process_form?
+    controller_name == 'experience' && action_name == 'application_form'
+  end
+
 end
