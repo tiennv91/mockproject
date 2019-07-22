e9ba471339b1ef4d357b20cbc5134e6ceb6927d3
duongtrang2701 <duong1997trang@gmail.com>
2019-07-22 10:00:38 +0700

    Fixblog

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -2,19 +2,18 @@
 
 class BlogController < ApplicationController
   impressionist
-  # helpe r_method :page_count
   def index
     # breadcrumb
     add_breadcrumb 'Blog', :blog_index_path
     @blogs = Blog.all.order(created_at: :DESC).page(params[:page]).per(3)
     @blog_first = Blog.all.order(created_at: :DESC).first
-   
+    @blog_count = Blog.count
+    @page = params[:page].to_i
     @popular_blogs = Blog.popular
 
     @search = Blog.ransack(params[:q])
     @categories = Category.all
     @locations = Location.all
-    @hashtags = Hashtag.all
     @search.sorts = 'blog_details.title desc' if @search.sorts.empty?
     @blogs = @search.result(distinct: true).order(created_at: :DESC).page(params[:page]).per(3)
 
@@ -58,12 +57,12 @@ class BlogController < ApplicationController
   def search
     unless params[:q][:categories_category_name_in].nil?
       params[:q][:categories_category_name_in] = params[:q][:categories_category_name_in].split(",")
-    end
-    # unless params[:q][:hashtags_tag_name_cont].nil?
-    #   params[:q][:hashtags_tag_name_cont] = params[:q][:hashtags_tag_name_cont].split(",")
-    # end
-    unless params[:q][:location_province_in].nil?
-      params[:q][:location_province_in] = params[:q][:location_province_in].split(",")
+      unless params[:q][:hashtags_tag_name_or_hashtags_tag_name_cont_any].nil?
+        params[:q][:hashtags_tag_name_or_hashtags_tag_name_cont_any] = params[:q][:hashtags_tag_name_or_hashtags_tag_name_cont_any].split(" ")
+        unless params[:q][:location_province_in].nil?
+          params[:q][:location_province_in] = params[:q][:location_province_in].split(",")
+        end
+      end
     end
     index
     render :index
