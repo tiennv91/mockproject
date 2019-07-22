2da31e6b978c77674f813957a308518af5b12573
duongtrang2701 <duong1997trang@gmail.com>
2019-07-22 12:05:38 +0700

    Fixblog

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -5,45 +5,22 @@ class BlogController < ApplicationController
   def index
     # breadcrumb
     add_breadcrumb 'Blog', :blog_index_path
-    @blogs = Blog.all.order(created_at: :DESC).page(params[:page]).per(3)
     @blog_first = Blog.all.order(created_at: :DESC).first
-    @blog_count = Blog.count
     @page = params[:page].to_i
     @popular_blogs = Blog.popular
 
     @search = Blog.ransack(params[:q])
     @categories = Category.all
     @locations = Location.all
-    @search.sorts = 'blog_details.title desc' if @search.sorts.empty?
+    @search.sorts = 'created_at desc' if @search.sorts.empty?
     @blogs = @search.result(distinct: true).order(created_at: :DESC).page(params[:page]).per(3)
-
+    
     respond_to do |format|
       format.html
       format.json { render json: @blogs }
     end
   end
-  
-  # def page_count
-  #   @blog_count = Blog.count
-  #   @page = params[:page].to_i
-  #   if @page == 0
-  #     if @blog_count < 3
-  #       @blog_count
-  #     else
-  #       3
-  #     end
-  #   elsif @page > 0
-  #     if @page == @blogs.total_pages
-  #       @blog_count
-  #     elsif @page > @blogs.total_pages
-  #       @page
-  #     else
-  #       3*@page
-  #     end
-  #   end
-  #   page_count
-  # end
-
+ 
   def show
     @blog = Blog.find(params[:id])
     @popular_blogs = Blog.popular
