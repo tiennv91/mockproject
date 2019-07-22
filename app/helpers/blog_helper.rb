dd79124bfc258d8ddcc6778fd7f27472856aac74
longdang0497 <danghoanglong3004@gmail.com>
2019-07-22 16:53:29 +0700

    update rank image blog

--- app/helpers/blog_helper.rb
+++ app/helpers/blog_helper.rb
@@ -1,2 +1,12 @@
 module BlogHelper
+  def rank(index)
+    case index
+    when 0
+      image_tag("blog/rank_icon01.png", class: "num_pic")
+    when 1
+      image_tag("blog/rank_icon02.png", class: "num_pic")
+    when 2
+      image_tag("blog/rank_icon03.png", class: "num_pic")
+    end 
+  end 
 end
