dcc0343e3d23c1ac2fe51648109cef655a2a4d43
dangnghia1997 <dangnghia25197@gmail.com>
2019-07-22 13:40:54 +0700

    modify

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -10,11 +10,11 @@ class BlogController < ApplicationController
     @blog_first = Blog.all.order(created_at: :DESC).first
    
     @popular_blogs = Blog.popular
-
-    @search = Blog.ransack(params[:q])
     @categories = Category.all
     @locations = Location.all
     @hashtags = Hashtag.all
+
+    @search = Blog.ransack(params[:q])
     @search.sorts = 'blog_details.title desc' if @search.sorts.empty?
     @blogs = @search.result(distinct: true).order(created_at: :DESC).page(params[:page]).per(3)
 
@@ -23,27 +23,6 @@ class BlogController < ApplicationController
       format.json { render json: @blogs }
     end
   end
-  
-  # def page_count
-  #   @blog_count = Blog.count
-  #   @page = params[:page].to_i
-  #   if @page == 0
-  #     if @blog_count < 3
-  #       @blog_count
-  #     else
-  #       3
-  #     end
-  #   elsif @page > 0
-  #     if @page == @blogs.total_pages
-  #       @blog_count
-  #     elsif @page > @blogs.total_pages
-  #       @page
-  #     else
-  #       3*@page
-  #     end
-  #   end
-  #   page_count
-  # end
 
   def show
     @blog = Blog.find(params[:id])
@@ -56,9 +35,8 @@ class BlogController < ApplicationController
   end
 
   def search
-    unless params[:q][:categories_category_name_in].nil?
-      params[:q][:categories_category_name_in] = params[:q][:categories_category_name_in].split(",")
-    end
+    
+    params[:q][:categories_category_name_in] = params[:q][:categories_category_name_in].split(",")
     unless params[:q][:hashtags_tag_name_in].nil?
       params[:q][:hashtags_tag_name_in] = params[:q][:hashtags_tag_name_in].split(",")
     end
