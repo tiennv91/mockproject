92aa79b1485082ad7b0646c8d4a471f114a2440f
longdang0497 <danghoanglong3004@gmail.com>
2019-07-24 12:32:13 +0700

    Fix merge Nghia

--- db/schema.rb
+++ db/schema.rb
@@ -80,36 +80,6 @@ ActiveRecord::Schema.define(version: 2019_07_23_085503) do
     t.index ["role_id"], name: "index_admin_users_on_role_id"
   end
 
-  create_table "admins", force: :cascade do |t|
-    t.string "fullname", default: ""
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.bigint "role_id"
-    t.string "avatar"
-    t.string "bio"
-    t.boolean "delete_flg", default: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet "current_sign_in_ip"
-    t.inet "last_sign_in_ip"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.integer "failed_attempts", default: 0, null: false
-    t.string "unlock_token"
-    t.datetime "locked_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_admins_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
-    t.index ["role_id"], name: "index_admins_on_role_id"
-  end
-
   create_table "blog_details", force: :cascade do |t|
     t.bigint "blog_id"
     t.string "title"
@@ -219,8 +189,10 @@ ActiveRecord::Schema.define(version: 2019_07_23_085503) do
 
   create_table "experiences", force: :cascade do |t|
     t.bigint "location_id"
+    t.bigint "admin_user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["admin_user_id"], name: "index_experiences_on_admin_user_id"
     t.index ["location_id"], name: "index_experiences_on_location_id"
   end
 
@@ -273,5 +245,6 @@ ActiveRecord::Schema.define(version: 2019_07_23_085503) do
   add_foreign_key "bookings", "experiences"
   add_foreign_key "category_experiences", "categories"
   add_foreign_key "category_experiences", "experiences"
+  add_foreign_key "experiences", "admin_users"
   add_foreign_key "experiences", "locations"
 end
